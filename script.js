e184ae9f16c9e67a38e23ed842478f6b306e3db2
marcio lira <machollanda@gmail.com>
2020-05-18 22:56:01 -0300

    criado alimento da snake

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake [0] = {
 	y: 8 * box
 }
 let direction = "right";
+let food = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 
 function criarBG(){
@@ -21,6 +25,12 @@ function criarSnake(){
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
+
+function drawFood(){
+	context.fillStyle = "#FF8C00";
+	context.fillRect(food.x, food.y, box, box);
+}
+
 
 document.addEventListener('keydown', update);
 
@@ -40,6 +50,7 @@ if(snake[0].y < 0 && box && direction == "up")snake[0].y = 16 * box;
 
 criarBG();
 criarSnake();
+drawFood();
 
 let snakeX = snake[0].x;
 let snakeY = snake[0].y;
